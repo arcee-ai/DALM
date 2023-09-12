293e9ee8b0462d16d7993b3d0aa380204db2ee66
shamanez <gayal.sha92@gmail.com>
2023-09-12 09:51:15 +0000

    Fix formatting

--- RAG_e2e_in_batch_negs/test_with_hnsw.py
+++ RAG_e2e_in_batch_negs/test_with_hnsw.py
@@ -11,13 +11,11 @@ import transformers
 from transformers import default_data_collator
 
 from accelerate.logging import get_logger
-from peft import PeftModel
 
 from base_model import AutoModelForRagE2E
 from test_utils import (
     preprocess_function,
     construct_search_index,
-    # get_query_embeddings,
     get_nearest_neighbours,
     calculate_precision_recall,
 )
@@ -117,14 +115,13 @@ def main():
     test_dataset = datasets.load_dataset(
         "csv", data_files={"test": f"{args.dataset_path}/triplets.csv"}
     )["test"]
-    
+
     # test_dataset = datasets.load_from_disk("/home/datasets/question_answer_pairs")
-    
+
     generator_tokenizer = rag_model.generator_tokenizer
     generator_tokenizer.pad_token = generator_tokenizer.eos_token
-    
+
     retriever_tokenizer = rag_model.retriever_tokenizer
-    
 
     processed_datasets = test_dataset.map(
         lambda example: preprocess_function(
@@ -140,8 +137,7 @@ def main():
         desc="Running tokenizer on dataset",
         num_proc=4,
     )
-    
- 
+
     unique_passages = set(processed_datasets[args.passage_column_name])
 
     def is_passage_unique(example):
@@ -150,9 +146,11 @@ def main():
         return is_in_unique_list
 
     unique_passage_dataset = processed_datasets.filter(is_passage_unique)
-    
-    passage_to_id_dict = {i: p[args.passage_column_name] for i, p in enumerate(unique_passage_dataset)}
-    
+
+    passage_to_id_dict = {
+        i: p[args.passage_column_name] for i, p in enumerate(unique_passage_dataset)
+    }
+
     unique_passage_dataloader = DataLoader(
         unique_passage_dataset,
         shuffle=False,
@@ -160,7 +158,7 @@ def main():
         batch_size=args.test_batch_size,
         pin_memory=True,
     )
-    
+
     # peft config and wrapping
     retriever_with_peft_layers = rag_model.retriever_model
     # (
@@ -179,63 +177,90 @@ def main():
     # )
 
     num_passages = len(unique_passage_dataset)
-    
+
     passage_embeddings_array = np.zeros((num_passages, args.embed_dim))
     for step, batch in enumerate(tqdm(unique_passage_dataloader)):
         with torch.no_grad():
             with torch.amp.autocast(dtype=torch.float16, device_type="cuda"):
-                passage_embs = rag_model.forward(
-                    "retrieval", 
-                    retriever_with_peft_layers, 
-                    batch["retriever_passage_input_ids"].to("cuda"), 
-                    batch["retriever_passage_attention_mask"].to("cuda")
-                ).detach().float().cpu().numpy()
-                
+                passage_embs = (
+                    rag_model.forward(
+                        "retrieval",
+                        retriever_with_peft_layers,
+                        batch["retriever_passage_input_ids"].to("cuda"),
+                        batch["retriever_passage_attention_mask"].to("cuda"),
+                    )
+                    .detach()
+                    .float()
+                    .cpu()
+                    .numpy()
+                )
+
         start_index = step * args.test_batch_size
-        end_index = start_index + args.test_batch_size if (start_index + args.test_batch_size) < num_passages else num_passages
+        end_index = (
+            start_index + args.test_batch_size
+            if (start_index + args.test_batch_size) < num_passages
+            else num_passages
+        )
         passage_embeddings_array[start_index:end_index] = passage_embs
         del passage_embs, batch
 
-    passage_search_index = construct_search_index(args.embed_dim, num_passages, passage_embeddings_array)
-    
+    passage_search_index = construct_search_index(
+        args.embed_dim, num_passages, passage_embeddings_array
+    )
+
     # Initialize counters
     batch_precision = []
     batch_recall = []
     total_hit = 0
-    
-    
+
     print("Retriever evaluation start")
-    
+
     # here we are interacting through the dataset, not a dataloader
     # so we need to convert them to a tensor
     # to do : convert this to batches by examples from the dataset to make it effcient
     # to:do : remove hard-coded args like device cpu  or cuda
     # to:do : torch_dtype make a varaibles float16 or bfloat16
-    # to:do :  remove bilterplate codes and create two functios to get_query_embeds and get_passage_embedins 
+    # to:do :  remove bilterplate codes and create two functios to get_query_embeds and get_passage_embedins
     for step, test_example in enumerate(processed_datasets):
         with torch.no_grad():
             with torch.amp.autocast(dtype=torch.float16, device_type="cuda"):
                 # use the batch size for the first dim
                 # do not hard-code it
-                retriever_query_input_ids = torch.tensor(test_example["retriever_query_input_ids"]).view(1, -1)
-                retriever_query__attention_mask = torch.tensor(test_example["retriever_query_attention_mask"]).view(1, -1)
-                
-                query_embeddings = rag_model.forward(
-                    "retrieval",
-                    retriever_with_peft_layers,
-                    retriever_query_input_ids.to("cuda"),
-                    retriever_query__attention_mask.to("cuda"),
-                ).detach().float().cpu().numpy()
-           
-   
-        search_results = get_nearest_neighbours(args.top_k, passage_search_index, query_embeddings, passage_to_id_dict, threshold=0.0)
-        
+                retriever_query_input_ids = torch.tensor(
+                    test_example["retriever_query_input_ids"]
+                ).view(1, -1)
+                retriever_query__attention_mask = torch.tensor(
+                    test_example["retriever_query_attention_mask"]
+                ).view(1, -1)
+
+                query_embeddings = (
+                    rag_model.forward(
+                        "retrieval",
+                        retriever_with_peft_layers,
+                        retriever_query_input_ids.to("cuda"),
+                        retriever_query__attention_mask.to("cuda"),
+                    )
+                    .detach()
+                    .float()
+                    .cpu()
+                    .numpy()
+                )
+
+        search_results = get_nearest_neighbours(
+            args.top_k,
+            passage_search_index,
+            query_embeddings,
+            passage_to_id_dict,
+            threshold=0.0,
+        )
+
         retrieved_passages = [item[0] for item in search_results]
-        
+
         correct_passages = [test_example[args.passage_column_name]]
-        
-   
-        precision, recall = calculate_precision_recall(retrieved_passages, correct_passages)
+
+        precision, recall = calculate_precision_recall(
+            retrieved_passages, correct_passages
+        )
 
         batch_precision.append(precision)
         batch_recall.append(recall)
@@ -247,55 +272,65 @@ def main():
     recall = sum(batch_recall) / total_examples
     precision = sum(batch_precision) / total_examples
     hit_rate = total_hit / float(total_examples)
-    
-    
+
     print("Retriever results:")
 
     print("Recall:", recall)
     print("Precision:", precision)
     print("Hit Rate:", hit_rate)
-    
-    
+
     print("*************")
     print(("Generator evaluation:"))
-    
+
     # for the geneator
-    total_em_hit = 0 # hinit use chatGPT to see what is Exact match when evalauting question answer models
+    total_em_hit = 0  # hinit use chatGPT to see what is Exact match when evalauting question answer models
     for test_example in processed_datasets:
-        
         # select query:
         with torch.no_grad():
             with torch.amp.autocast(dtype=torch.float16, device_type="cuda"):
                 # use the batch size for the first dim
                 # do not hard-code it
-                retriever_query_input_ids = torch.tensor(test_example["retriever_query_input_ids"]).view(1, -1)
-                retriever_query__attention_mask = torch.tensor(test_example["retriever_query_attention_mask"]).view(1, -1)
-                
-                query_embeddings = rag_model.forward(
-                    "retrieval",
-                    retriever_with_peft_layers,
-                    retriever_query_input_ids.to("cuda"),
-                    retriever_query__attention_mask.to("cuda"),
-                ).detach().float().cpu().numpy()
-           
-   
-        search_result_passage = get_nearest_neighbours(args.top_k, passage_search_index, query_embeddings, passage_to_id_dict, threshold=0.0)[0][0]
-        
+                retriever_query_input_ids = torch.tensor(
+                    test_example["retriever_query_input_ids"]
+                ).view(1, -1)
+                retriever_query__attention_mask = torch.tensor(
+                    test_example["retriever_query_attention_mask"]
+                ).view(1, -1)
+
+                query_embeddings = (
+                    rag_model.forward(
+                        "retrieval",
+                        retriever_with_peft_layers,
+                        retriever_query_input_ids.to("cuda"),
+                        retriever_query__attention_mask.to("cuda"),
+                    )
+                    .detach()
+                    .float()
+                    .cpu()
+                    .numpy()
+                )
+
+        search_result_passage = get_nearest_neighbours(
+            args.top_k,
+            passage_search_index,
+            query_embeddings,
+            passage_to_id_dict,
+            threshold=0.0,
+        )[0][0]
+
         # this query comes without the answer
         query = f"#query# {test_example[args.query_column_name]} #passage# {search_result_passage} #answer# "
-        
+
         answer = test_example[args.answer_column_name]
-        
-      
+
         pipeline = transformers.pipeline(
             "text-generation",
             model=generator_with_peft_layers,
-            tokenizer= rag_model.generator_tokenizer,
+            tokenizer=rag_model.generator_tokenizer,
             torch_dtype=torch.float16,
             trust_remote_code=True,
             device_map="auto",
         )
-        
 
         sequences = pipeline(
             query,
@@ -303,20 +338,15 @@ def main():
             do_sample=True,
             top_k=10,
             num_return_sequences=1,
-            eos_token_id= rag_model.generator_tokenizer.eos_token_id,
+            eos_token_id=rag_model.generator_tokenizer.eos_token_id,
         )
-        
-        
-    
-        generated_answer_string = sequences[0]["generated_text"].split("#answer#")[1]
-        
-        if generated_answer_string == answer:
-            total_em_hit =  total_em_hit +1
-            
-        
-    print("Exact mactch:", total_em_hit/len(processed_datasets))
-    
 
+        generated_answer_string = sequences[0]["generated_text"].split("#answer#")[1]
+
+        if generated_answer_string == answer:
+            total_em_hit = total_em_hit + 1
+
+    print("Exact mactch:", total_em_hit / len(processed_datasets))
 
 
 if __name__ == "__main__":
