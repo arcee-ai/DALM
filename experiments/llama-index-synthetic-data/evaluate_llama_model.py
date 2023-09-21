083b90036dd751ec25886a43a4da3fef79a1b58e
Ben Epstein <ben.epstein97@gmail.com>
2023-09-21 17:41:35 -0400

    debugging

--- experiments/llama-index-synthetic-data/evaluate_llama_model.py
+++ experiments/llama-index-synthetic-data/evaluate_llama_model.py
@@ -3,15 +3,22 @@ from llama_index.finetuning import EmbeddingQAFinetuneDataset
 
 from eval_utils import evaluate, display_results
 
+
 def run_eval(val_data: str) -> None:
 	val_dataset = EmbeddingQAFinetuneDataset.from_json(val_data)
+	print("Loading model")
 	embed_model_name = "local:BAAI/bge-small-en"
 	base_embed_model = resolve_embed_model("local:BAAI/bge-small-en")
-	embed_model = LinearAdapterEmbeddingModel(base_embed_model, "model_output_test")
+	print("model loaded")
+	print("loading adapter")
+	embed_model = LinearAdapterEmbeddingModel(base_embed_model, "model_output_test", device="cuda")
+	print("adapter loaded")
 	# Top k 10 to match our internal experiments
+	print("evaluating base model")
 	bge_val_results = evaluate(val_dataset, embed_model_name, top_k=10)
 	print("Base Model Results:")
 	display_results(["bge"], [bge_val_results])
+	print("evaluating finetuned model")
 	ft_val_results = evaluate(val_dataset, embed_model)
 	print("Fine-Tuned Model Results")
 	display_results(["ft"], [ft_val_results])
@@ -19,7 +26,6 @@ def run_eval(val_data: str) -> None:
 	display_results(
 		["bge", "ft"], [bge_val_results, ft_val_results]
 	)
-
 
 
 
