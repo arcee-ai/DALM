2b3f15ab56d11c9f72c7c51694c99621a0bf2093
Ben Epstein <ben.epstein97@gmail.com>
2023-09-21 17:42:59 -0400

    test

--- experiments/llama-index-synthetic-data/evaluate_llama_model.py
+++ experiments/llama-index-synthetic-data/evaluate_llama_model.py
@@ -14,14 +14,15 @@ def run_eval(val_data: str) -> None:
 	embed_model = LinearAdapterEmbeddingModel(base_embed_model, "model_output_test", device="cuda")
 	print("adapter loaded")
 	# Top k 10 to match our internal experiments
+	print("evaluating finetuned model")
+	ft_val_results = evaluate(val_dataset, embed_model, top_k=10)
+	print("Fine-Tuned Model Results")
+	display_results(["ft"], [ft_val_results])
+
 	print("evaluating base model")
 	bge_val_results = evaluate(val_dataset, embed_model_name, top_k=10)
 	print("Base Model Results:")
 	display_results(["bge"], [bge_val_results])
-	print("evaluating finetuned model")
-	ft_val_results = evaluate(val_dataset, embed_model)
-	print("Fine-Tuned Model Results")
-	display_results(["ft"], [ft_val_results])
 	print("All Results")
 	display_results(
 		["bge", "ft"], [bge_val_results, ft_val_results]
