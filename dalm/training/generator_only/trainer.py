42f77f5741223ba80e9fd28b0b75aef08f375ee3
Luke Meyers <functor.soup@gmail.com>
2023-11-20 23:07:38 -0500

    Trainer script cleanup and corrections

--- dalm/training/generator_only/trainer.py
+++ dalm/training/generator_only/trainer.py
@@ -16,6 +16,7 @@ import argparse
 def create_datasets(
     dataset_name: str,
     split: str,
+    validation_split: Optional[float],
     size_valid_set: Optional[int],
     streaming: bool,
     shuffle_buffer: int,
@@ -39,7 +40,7 @@ def create_datasets(
         train_data = dataset.skip(size_valid_set)
         train_data = train_data.shuffle(buffer_size=shuffle_buffer, seed=None)
     else:
-        dataset = dataset.train_test_split(test_size=0.05, seed=None)
+        dataset = dataset.train_test_split(test_size=validation_split, seed=None)
         train_data = dataset["train"]
         valid_data = dataset["test"]
         print(f"Size of the train set: {len(train_data)}. Size of the validation set: {len(valid_data)}")
@@ -67,9 +68,11 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_name", type=str, default="HuggingFaceH4/zephyr-7b-alpha", help="the model name")
     parser.add_argument("--log_with", type=str, default="wandb", help="use 'wandb' to log with wandb")
-    parser.add_argument("--dataset_name", type=str, required=True, help="The dataset name either corresponding repo or  Format should be that of ChatML")
+    parser.add_argument("--dataset_name", type=str, required=True, help="The dataset name corresponding to one sitting on huggingface or a local one. If local, be sure to set the local_dataset flag")
+    parser.add_argument("--local_dataset", type=bool, default=False, help="whether to load the dataset from disk")
     parser.add_argument("--split", type=str, default="train", help="the split to use")
-    parser.add_argument("--size_valid_set", type=int, default=4000, help="the size of the validation set")
+    parser.add_argument("--size_valid_set", type=int, default=4000, help="the size of the validation set (when streaming is enabled)")
+    parser.add_argument("--validation_split", type=float, default=0.05, help="the validation split percentage")
     parser.add_argument("--streaming", type=bool, default=False, help="whether to stream the dataset")
     parser.add_argument("--shuffle_buffer", type=int, default=5000, help="the shuffle buffer size")
     parser.add_argument("--seq_length", type=int, default=2600, help="the sequence length")
@@ -99,40 +102,42 @@ def parse_args():
 
     parser.add_argument("--output_dir", type=str, default="./generator_finetuned_model", help="the output directory")
     parser.add_argument("--neftune_noise_alpha", type=int, default=5, help="the noise alpha for neftune")
-    parser.add_argument("--log_freq", type=int, default=1, help="the logging frequency")
+    parser.add_argument("--run_name", type=str, default="generator_finetuning", help="the tracker run name")
     return parser.parse_args()
 
 
 def train_generator(
-    model_name,
-    dataset_name,
-    num_train_epochs,
-    split,
-    size_valid_set,
-    streaming,
-    shuffle_buffer,
-    seq_length,
-    num_workers,
-    eval_steps,
-    logging_steps,
-    per_device_train_batch_size,
-    per_device_eval_batch_size,
-    gradient_accumulation_steps,
-    gradient_checkpointing,
-    group_by_length,
-    packing,
-    lora_alpha,
-    lora_dropout,
-    lora_r,
-    learning_rate,
-    lr_scheduler_type,
-    num_warmup_steps,
-    weight_decay,
-    optimizer_type,
-    output_dir,
-    log_steps,
-    neftune_noise_alpha=5,
-    log_with="wandb",
+    model_name: str,
+    dataset_name: str,
+    local_dataset: bool,
+    num_train_epochs: int,
+    split: str,
+    size_valid_set: Optional[int],
+    validation_split: Optional[float],
+    streaming: bool,
+    shuffle_buffer: Optional[int],
+    seq_length: int,
+    num_workers:int,
+    eval_steps:int,
+    logging_steps:int,
+    per_device_train_batch_size:int,
+    per_device_eval_batch_size:int,
+    gradient_accumulation_steps:int,
+    gradient_checkpointing:int,
+    group_by_length:int,
+    packing:bool,
+    lora_alpha:int,
+    lora_dropout:float,
+    lora_r:int,
+    learning_rate:float,
+    lr_scheduler_type:float,
+    num_warmup_steps:int,
+    weight_decay:float,
+    optimizer_type:str,
+    output_dir:str,
+    neftune_noise_alpha:int,
+    log_with:str,
+    run_name:str,
 ):
     tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
     tokenizer.pad_token = tokenizer.eos_token
@@ -178,9 +183,8 @@ def train_generator(
         optim=optimizer_type,
         bf16=True,
         remove_unused_columns=False,
-        run_name="generator_tuning",
+        run_name=run_name,
         weight_decay=weight_decay,
-        logging_steps=log_steps,
         neftune_noise_alpha=neftune_noise_alpha,
     )
 
@@ -192,10 +196,12 @@ def train_generator(
     train_dataset, eval_dataset = create_datasets(
         dataset_name,
         split,
+        validation_split,
         size_valid_set,
         streaming,
         shuffle_buffer,
         num_workers,
+        local_dataset
     )
 
     chars_per_token = chars_token_ratio(train_dataset, tokenizer, prepare_sample_text)
@@ -225,8 +231,10 @@ def main():
     train_generator(
         model_name=args.model_name,
         dataset_name=args.dataset_name,
+        local_dataset=args.local_dataset,
         split=args.split,
         size_valid_set=args.size_valid_set,
+        validation_split=args.validation_split,
         streaming=args.streaming,
         shuffle_buffer=args.shuffle_buffer,
         seq_length=args.seq_length,
@@ -249,9 +257,9 @@ def main():
         weight_decay=args.weight_decay,
         optimizer_type=args.optimizer_type,
         output_dir=args.output_dir,
-        log_steps=args.log_steps,
         log_with=args.log_with,
         neftune_noise_alpha=args.neftune_noise_alpha,
+        run_name=args.run_name,
     )
 
 
