567c910436113316c13ac175f695b2b6f195ad0b
Luke Meyers <functor.soup@gmail.com>
2023-12-04 06:39:58 -0500

    Da 24/reading comprehension (#74)
    * Broken reading-comprehension generators and generator training
    
    * Refactor out shared utils and add domain tokenizer training as a option in direct script tun
    
    * Corrections
    
    * Further corrections
    
    * Add q&a extractor as a util and to the pipeline example script
    
    * Util correction and context addition to output
    
    * Revert previous corrections
    
    * Chatml outputformat for regex based rc and remove domain keyword
    
    * Generator additions
    
    * More generator corrections and additions
    
    * Add train num epochs as an arg to the generator training script
    
    * Add new dependencies
    
    * 1. json dump when writing to file
    2. regex-based-gen now creates a domain tokenizer if both domain sentencepiece model and domain text (explicitly) is not given
    3. attempt at pipeline
    
    * Post pipeline test run corrections
    
    * Remove (explicit) use of ConstantLengthDataset
    
    * Lift state management to a higer level and some corrections
    
    * Add option to save dataset as huggingface dataset
    
    * Training script corrections
    
    * Trainer script cleanup and corrections
    
    * Add cloud friendly logger
    
    * Reformatted synthetic dataset generation + corrections
    
    * More formatting for the synth-gen script
    
    * More corrections to synth-gen
    
    * Regex-gen changes and banner addition
    
    * Missing comma
    
    * Type hint all the functions in utils
    
    * Lightly refactor the pipeline
    
    * Address proper negation of lags
    
    * Switch out generator for iterator when type hinting
    
    * Util typing correction
    
    * Correct all linting issues
    
    * Pipeline corrections
    
    * More corrections for output type of generator
    
    * More corrections to the pipeline
    
    * Appeasing the linter for the pipeline code
    
    * Appeasing the linter for llm synth script
    
    * Appeasing the linter for the training script
    
    * Linter based corrections for utils
    
    * More appeasing of the linter and work arounds
    
    * Incorporate csv reading and associated changes
    
    * Unicode decoding revisit
    
    * More fixes
    
    * Forgot to put in replace line
    
    * Better logging and removal of statefile and more corrections
    
    * Add missing general spm input validation line to pipeline script
    
    * More validation lines for pipeline
    
    * More corrections
    
    * Banner correction, corrections
    
    * Start of README.md and add general sentencepiece model to resources
    
    * Add defaults for cli args
    
    * Add more detail to README.md
    
    * Add defaults to function
    
    * Defaults
    
    * README.md for rc pipeline
    
    * transformers version dependency constraint
    
    * alpha -> beta
    
    * Better warning message
    
    * Correct description in README.md
    
    * Stream arg correction for trainer
    
    * Add prompt link to README
    
    * Add general spm to resources
    
    * - Better input content generator (deals with directory of csv(s))
    - remove default wandb value
    - new logging statement
    - change of terminology for logging (files -> texts)
    
    * Vocab size second-try and key error fix
    
    * Correct logging
    
    * Update dalm/pipelines/reading_comprehension_pipeline.py
    
    Co-authored-by: Traun Leyden <traun.leyden@gmail.com>
    
    * Update dalm/pipelines/reading_comprehension_pipeline.py
    
    Co-authored-by: Traun Leyden <traun.leyden@gmail.com>
    
    * Update dalm/datasets/reading_comprehension_generation/synthetic_based.py
    
    Co-authored-by: Traun Leyden <traun.leyden@gmail.com>
    
    * Update dalm/datasets/reading_comprehension_generation/utils.py
    
    Co-authored-by: Traun Leyden <traun.leyden@gmail.com>
    
    * Update dalm/pipelines/reading_comprehension_pipeline.py
    
    Co-authored-by: Traun Leyden <traun.leyden@gmail.com>
    
    * Update dalm/pipelines/reading_comprehension_pipeline.py
    
    Co-authored-by: Traun Leyden <traun.leyden@gmail.com>
    
    * Corrections
    
    * Post linting
    
    * Update README with suggested corrections
    
    * grammar corrections
    
    ---------
    
    Co-authored-by: Traun Leyden <traun.leyden@gmail.com>

---
+++ dalm/datasets/reading_comprehension_generation/README.md
@@ -0,0 +1,49 @@
+## A note about reading comprehension
+
+This aproach of adapting LLMs is based on this [paper](https://arxiv.org/abs/2309.09530) by Microsoft 
+The way espoused by the paper is generating reading comprehension questions and answers based on the raw corpora
+and training a llm on said generated dataset can enhance its domain adaptiveness
+
+We have two ways of generating reading comprehension data
+
+1. Via regex based methods that combs the input data for match and aligns them into questions and answers
+2. Via prompting a large language model to come up with questions and answers 
+
+To see the prompt behind LLM based reading-comprehension dataset generation please go [here](https://github.com/arcee-ai/DALM/blob/4d93d4a198cc64ce5d19ee98786b70f579dbef0c/dalm/datasets/reading_comprehension_generation/synthetic_based.py#L22)
+
+## How to get started
+
+For the input, either a single csv file or a directory of individual files each containing raw text will do.
+
+
+### LLM based
+
+Assuming you have your dataset as a csv file with the column `text` containing the raw texts
+
+(chunking based on context length of model is enabled by default) 
+
+```bash
+python dalm/datasets/reading_comprehension_generation/synthetic_based.py \
+            --model HuggingFaceH4/zephyr-7b-alpha \
+            --context-length  4192
+            --input input_dataset.csv  --output_directory synth_data --dataset_name llm_generated_dataset
+```
+
+the output directory serves as a temporary holding place of all generated data before it can be made a dataset.
+The generation process is time consuming and expensive. On average, because the process uses a LLM (if using the recommended 13b llama2 model), it takes about 20-30 minutes to produce 10 questions (numbers may vary depending on the content of your dataset and the unpredictability of the model). So every step is taken to ensure that if the process is interrupted, once back running will pick up where it left off. 
+
+Chunking of data is enabled by default and requires the context length to be passed  which is why it passed in in the example
+
+### Regex based
+
+(Same, as above i.e assuming you have your dataset as a csv file with the column `text` containing the raw texts)
+
+Please note there is the choice of passing in a domain sentence model in addition, but this is not required as
+the script will train a domain specific sentencepiece model on the input corpus
+
+```bash
+
+python dalm/datasets/reading_comprehension_generation/regex_based.py  --input input.csv \
+            --csv_column text --general_spm_path resources/general.spm  \
+            --output_dataset_name regex_dataset
+```
---
+++ dalm/training/generator_only/trainer.py
@@ -0,0 +1,300 @@
+import argparse
+import logging
+import os
+from typing import Any, Callable, Dict, Optional, Tuple
+
+import torch
+from accelerate import Accelerator
+from datasets import Dataset, load_dataset, load_from_disk
+from peft import LoraConfig
+from tqdm import tqdm
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    PreTrainedTokenizerBase,
+    TrainingArguments,
+)
+from trl import SFTTrainer  # type: ignore[import]
+
+logger = logging.getLogger(__name__)
+
+
+def create_datasets(
+    dataset_name: str,
+    split: str,
+    validation_split: Optional[float],
+    size_valid_set: Optional[int],
+    streaming: bool,
+    shuffle_buffer: Optional[int],
+    num_workers: int,
+    local_dataset: bool = False,
+) -> Tuple[Dataset, Dataset]:
+    if local_dataset:
+        dataset = load_from_disk(
+            dataset_name,
+        )
+    else:
+        dataset = load_dataset(
+            dataset_name,
+            split=split,
+            num_proc=num_workers if not streaming else None,
+            streaming=streaming,
+        )
+    if streaming:
+        logging.info("Loading the dataset in streaming mode")
+        if not (shuffle_buffer and size_valid_set):
+            raise ValueError("size_valid_set must be set when streaming is enabled")
+        valid_data = dataset.take(size_valid_set)
+        train_data = dataset.skip(size_valid_set)
+        train_data = train_data.shuffle(buffer_size=shuffle_buffer, seed=None)
+    else:
+        if not validation_split:
+            raise ValueError("validation_split must be set when streaming is disabled")
+        dataset = dataset.train_test_split(test_size=validation_split, seed=None)
+        train_data = dataset["train"]
+        valid_data = dataset["test"]
+        logging.info(f"Size of the train set: {len(train_data)}. Size of the validation set: {len(valid_data)}")
+
+    return train_data, valid_data
+
+
+def chars_token_ratio(
+    dataset: Dataset,
+    tokenizer: PreTrainedTokenizerBase,
+    formatting_func: Callable[[Dict[str, Any]], str],
+    sample_size: int = 400,
+) -> float:
+    """
+    Estimate the average number of characters per token in the dataset.
+    """
+    total_characters, total_tokens = 0, 0
+    for _, example in tqdm(zip(range(sample_size), iter(dataset), strict=False), total=sample_size):
+        text = formatting_func(example)
+        total_characters += len(text)
+        if tokenizer.is_fast:
+            total_tokens += len(tokenizer(text).tokens())
+        else:
+            total_tokens += len(tokenizer.tokenize(text))
+
+    return total_characters / total_tokens
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_name", type=str, default="HuggingFaceH4/zephyr-7b-beta", help="the model name")
+    parser.add_argument("--log_with", type=str, help="tracker name (wandb, mlflow, ..etc)")
+    parser.add_argument(
+        "--dataset_name",
+        type=str,
+        required=True,
+        help=(
+            "The dataset name corresponding to one sitting on huggingface or a local one"
+            "If local, be sure to set the local_dataset flag"
+        ),
+    )
+    parser.add_argument("--local_dataset", action="store_true", help="whether to use a local dataset")
+    parser.add_argument("--split", type=str, default="train", help="the split to use")
+    parser.add_argument(
+        "--size_valid_set", type=int, default=4000, help="the size of the validation set (when streaming is enabled)"
+    )
+    parser.add_argument("--validation_split", type=float, default=0.05, help="the validation split percentage")
+    parser.add_argument("--stream", action="store_true", help="whether to stream the dataset")
+    parser.add_argument("--shuffle_buffer", type=int, default=5000, help="the shuffle buffer size")
+    parser.add_argument("--seq_length", type=int, default=2600, help="the sequence length")
+    parser.add_argument("--num_workers", type=int, default=4, help="the number of workers")
+
+    parser.add_argument("--eval_steps", type=int, default=200, help="the evaluation frequency")
+    parser.add_argument("--num_train_epochs", type=int, default=3, help="the number of training epochs")
+    parser.add_argument("--logging_steps", type=int, default=10, help="the logging frequency")
+    parser.add_argument("--per_device_train_batch_size", type=int, default=1, help="the per device train batch size")
+    parser.add_argument("--per_device_eval_batch_size", type=int, default=1, help="the per device eval batch size")
+    parser.add_argument("--gradient_accumulation_steps", type=int, default=32, help="the gradient accumulation steps")
+    parser.add_argument(
+        "--gradient_checkpointing", type=bool, default=True, help="whether to use gradient checkpointing"
+    )
+    parser.add_argument("--group_by_length", type=bool, default=False, help="whether to group by length")
+    parser.add_argument("--no-packing", action="store_true", help="whether to not pack the sequences")
+
+    parser.add_argument("--lora_alpha", type=float, default=512, help="the lora alpha parameter")
+    parser.add_argument("--lora_dropout", type=float, default=0.05, help="the lora dropout parameter")
+    parser.add_argument("--lora_r", type=int, default=256, help="the lora r parameter")
+
+    parser.add_argument("--learning_rate", type=float, default=1e-4, help="the learning rate")
+    parser.add_argument("--lr_scheduler_type", type=str, default="cosine", help="the lr scheduler type")
+    parser.add_argument("--num_warmup_steps", type=int, default=100, help="the number of warmup steps")
+    parser.add_argument("--weight_decay", type=float, default=0.05, help="weight decay")
+    parser.add_argument("--optimizer_type", type=str, default="paged_adamw_32bit", help="the optimizer type")
+
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        default="./generator_finetuned_model",
+        help="the output directory where the model will be saved",
+    )
+    parser.add_argument("--neftune_noise_alpha", type=int, default=5, help="the noise alpha for neftune")
+    parser.add_argument("--run_name", type=str, default="generator_finetuning", help="the tracker run name")
+    return parser.parse_args()
+
+
+def train_generator(
+    model_name: str,
+    dataset_name: str,
+    local_dataset: bool,
+    run_name: str,
+    output_dir: str,
+    log_with: Optional[str],
+    size_valid_set: Optional[int],
+    validation_split: Optional[float],
+    shuffle_buffer: Optional[int],
+    num_train_epochs: int = 1,
+    split: str = "train",
+    streaming: bool = False,
+    seq_length: int = 2600,
+    num_workers: int = 4,
+    eval_steps: int = 200,
+    logging_steps: int = 10,
+    per_device_train_batch_size: int = 1,
+    per_device_eval_batch_size: int = 1,
+    gradient_accumulation_steps: int = 1,
+    gradient_checkpointing: bool = True,
+    group_by_length: bool = False,
+    packing: bool = True,
+    lora_alpha: int = 512,
+    lora_dropout: float = 0.05,
+    lora_r: int = 256,
+    learning_rate: float = 1e-4,
+    lr_scheduler_type: str = "cosine",
+    num_warmup_steps: int = 100,
+    weight_decay: float = 0.05,
+    optimizer_type: str = "paged_adamw_32bit",
+    neftune_noise_alpha: int = 5,
+) -> None:
+    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.padding_side = "right"  # Fix weird overflow issue with fp16 training
+
+    bnb_config = BitsAndBytesConfig(load_in_4bit=True, bnb_4bit_quant_type="nf4", bnb_4bit_compute_dtype=torch.bfloat16)
+
+    base_model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        quantization_config=bnb_config,
+        device_map={"": Accelerator().local_process_index},
+        trust_remote_code=True,
+    )
+
+    base_model.config.use_cache = False
+
+    peft_config = LoraConfig(
+        r=lora_r,
+        lora_alpha=lora_alpha,
+        lora_dropout=lora_dropout,
+        target_modules=["q_proj", "v_proj"],
+        bias="none",
+        task_type="CAUSAL_LM",
+    )
+
+    training_args = TrainingArguments(
+        output_dir=output_dir,
+        per_device_train_batch_size=per_device_train_batch_size,
+        gradient_accumulation_steps=gradient_accumulation_steps,
+        gradient_checkpointing=gradient_checkpointing,
+        per_device_eval_batch_size=per_device_eval_batch_size,
+        learning_rate=learning_rate,
+        logging_steps=logging_steps,
+        num_train_epochs=num_train_epochs,
+        report_to=log_with,
+        save_strategy="epoch",
+        evaluation_strategy="steps",
+        eval_steps=eval_steps,
+        group_by_length=group_by_length,
+        lr_scheduler_type=lr_scheduler_type,
+        warmup_steps=num_warmup_steps,
+        optim=optimizer_type,
+        bf16=True,
+        remove_unused_columns=False,
+        run_name=run_name,
+        weight_decay=weight_decay,
+        neftune_noise_alpha=neftune_noise_alpha,
+    )
+
+    def prepare_sample_text(example: Dict[str, Any]) -> str:
+        """Prepare the text from a sample of the dataset."""
+        text = tokenizer.apply_chat_template(example["messages"], tokenize=False)
+        return text
+
+    train_dataset, eval_dataset = create_datasets(
+        dataset_name=dataset_name,
+        split=split,
+        validation_split=validation_split,
+        size_valid_set=size_valid_set,
+        streaming=streaming,
+        shuffle_buffer=shuffle_buffer,
+        num_workers=num_workers,
+        local_dataset=local_dataset,
+    )
+
+    chars_per_token = chars_token_ratio(train_dataset, tokenizer, prepare_sample_text)
+    logging.info(f"The character to token ratio of the dataset is: {chars_per_token:.2f}")
+
+    logging.info("Starting the model training")
+
+    trainer = SFTTrainer(
+        model=base_model,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        peft_config=peft_config,
+        packing=packing,
+        max_seq_length=seq_length,
+        tokenizer=tokenizer,
+        args=training_args,
+        chars_per_token=chars_per_token,
+        formatting_func=prepare_sample_text,
+    )
+
+    trainer.train()
+    trainer.save_model(output_dir)
+
+    output_dir = os.path.join(output_dir, "final_checkpoint")
+    trainer.model.save_pretrained(output_dir)
+
+
+def main() -> None:
+    args = parse_args()
+    train_generator(
+        model_name=args.model_name,
+        dataset_name=args.dataset_name,
+        local_dataset=args.local_dataset,
+        split=args.split,
+        size_valid_set=args.size_valid_set,
+        validation_split=args.validation_split,
+        streaming=args.stream,
+        shuffle_buffer=args.shuffle_buffer,
+        seq_length=args.seq_length,
+        num_workers=args.num_workers,
+        num_train_epochs=args.num_train_epochs,
+        eval_steps=args.eval_steps,
+        logging_steps=args.logging_steps,
+        per_device_train_batch_size=args.per_device_train_batch_size,
+        per_device_eval_batch_size=args.per_device_eval_batch_size,
+        gradient_accumulation_steps=args.gradient_accumulation_steps,
+        gradient_checkpointing=args.gradient_checkpointing,
+        group_by_length=args.group_by_length,
+        packing=not args.no_packing,
+        lora_alpha=args.lora_alpha,
+        lora_dropout=args.lora_dropout,
+        lora_r=args.lora_r,
+        learning_rate=args.learning_rate,
+        lr_scheduler_type=args.lr_scheduler_type,
+        num_warmup_steps=args.num_warmup_steps,
+        weight_decay=args.weight_decay,
+        optimizer_type=args.optimizer_type,
+        output_dir=args.output_dir,
+        log_with=args.log_with,
+        neftune_noise_alpha=args.neftune_noise_alpha,
+        run_name=args.run_name,
+    )
+
+
+if __name__ == "__main__":
+    main()
